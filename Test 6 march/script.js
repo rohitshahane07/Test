4dfae69d2944147646cb6d91bf734c1f387aabbc
rohitshahane07 <shahanerohit200@gmail.com>
2022-03-06 15:00:50 +0530

    test

---
+++ Test 6 march/script.js
@@ -0,0 +1,74 @@
+// alert('gh')
+
+
+
+
+
+function mobile1(){
+   var a = document.getElementById('m32');
+    a.classList.add('show')
+
+    var b = document.getElementById('m52');
+    b.classList.remove('show')
+   
+    var c = document.getElementById('redmi9a');
+    c.classList.remove('show')
+
+    var d = document.getElementById('redmi9');
+     d.classList.remove('show')
+
+    
+}
+
+function mobile2(){
+    var b = document.getElementById('m52');
+     b.classList.add('show')
+
+     var a = document.getElementById('m32');
+     a.classList.remove('show')
+    
+     var c = document.getElementById('redmi9a');
+     c.classList.remove('show')
+ 
+     var d = document.getElementById('redmi9');
+      d.classList.remove('show')
+ 
+    
+    
+     
+ }
+
+
+ function mobile3(){
+    var c = document.getElementById('redmi9a');
+     c.classList.add('show')
+
+     var b = document.getElementById('m52');
+     b.classList.remove('show')
+    
+     var a = document.getElementById('m32');
+    a.classList.remove('show')
+ 
+     var d = document.getElementById('redmi9');
+      d.classList.remove('show')
+ 
+     
+ }
+
+
+
+ function mobile4(){
+    var d = document.getElementById('redmi9');
+     d.classList.add('show')
+
+     var b = document.getElementById('m52');
+     b.classList.remove('show')
+    
+     var c = document.getElementById('redmi9a');
+     c.classList.remove('show')
+ 
+     var a = document.getElementById('m32');
+     a.classList.remove('show')
+     
+ }
+
